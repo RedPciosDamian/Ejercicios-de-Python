3bf4184d469b1a947d81bf7e3384c9c0461957eb
RedPciosDamian <redeterminacionesdepreciosrdi@gmail.com>
2025-06-02 11:39:04 -0300

    lista-tupla-diccionario

---
+++ lista_tupla_diccionario.py
@@ -0,0 +1,21 @@
+#lista se define con CORCHETES
+lista = [1, 2, 3, "aca va aun texto", True]
+print(lista)
+
+#TUPLA se define con PARENTESIS
+tupla = (1, 2, 3, "aca va aun texto", True)
+print(tupla)
+
+#DICCIONARIO se define con LLAVES. Puede contener otros diccionarios.
+diccionario = {
+    "clave": "valor",
+    "numero": 10,
+    "bool": True,
+    "dicc2": {
+        "otra clave": "algo",
+        "otro_num": 5
+    }
+}
+print(diccionario)
+
+
