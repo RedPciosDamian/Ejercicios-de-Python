6df2a98a6898aa7567111475381df4cf541d2cc2
RedPciosDamian <redeterminacionesdepreciosrdi@gmail.com>
2025-06-16 13:07:17 -0300

    Add files via upload
    Ejercicios de Python

---
+++ ejercitaciones de python/ejercicios_introduccion/sumar.py
@@ -0,0 +1,10 @@
+
+a = 62
+b = 6
+suma = a + b
+print(f"la suma de {a} + {b} es igual a {suma}") # asi se utiliza el format para imprimir.
+
+print(f"la variable a = {a} es:"), print(type(a)) # asi se utiliza el format para imprimir.
+print(f"la variable b = {b} es:"), print(type(b)) # asi se utiliza el format para imprimir.
+
+
---
+++ ejercitaciones de python/ejercicios_introduccion/variables.py
@@ -0,0 +1,24 @@
+print("hola mundo") #se usa para imprimir en pantalla
+
+variable = "hola mundo" # se usa para almacenar una variable string
+print(variable)
+print(type(variable))
+
+numero = 42 #numero entero (int)
+print(numero)
+print(type(numero))
+
+decimal = 10.2  # decimal (float)
+print(decimal)
+print(type(decimal))
+
+verdadero = True # booleano
+print(verdadero)
+print(type(verdadero))
+
+falso = False # booleano
+print(falso)
+print(type(falso))
+
+
+
