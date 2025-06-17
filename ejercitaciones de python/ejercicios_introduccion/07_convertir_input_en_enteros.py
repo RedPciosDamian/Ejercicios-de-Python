c50d0e6ee970e8dae8076a2fb939be0b6e01825c
RedPciosDamian <redeterminacionesdepreciosrdi@gmail.com>
2025-06-17 12:14:44 -0300

    Add files via upload
    Completando los ejercicios de Python

---
+++ ejercitaciones de python/ejercicios_introduccion/07_convertir_input_en_enteros.py
@@ -0,0 +1,12 @@
+edad = input("ingresa tu edad:")
+
+print(type(edad)) #type sirve para saber que tipo de dato es
+
+numero = int(edad) #int trasforma a entero
+# tambien tenemos los 
+# str tranforma a texto
+# float a numero con coma ej 32.4
+# bool a verdadero(true) falso(false)
+
+
+print(numero + 2)
